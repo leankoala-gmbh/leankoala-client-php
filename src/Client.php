ade1476fec6bef89058e60270a8510ca7aed56e7
Nils Langner <nils.langner@leankoala.com>
2021-06-30 22:32:32 +0200

    update: auth endpoints

--- src/Client.php
+++ src/Client.php
@@ -89,8 +89,8 @@ class Client
     private $servers = [
         self::ENVIRONMENT_DEV => 'http://localhost:8082/',
         self::ENVIRONMENT_LOCAL => 'http://localhost/',
-        self::ENVIRONMENT_STAGE => 'https://stage.monitor.leankoala.com/kapi/',
-        self::ENVIRONMENT_PRODUCTION => 'https://api.cluster1.koalityengine.com/'
+        self::ENVIRONMENT_STAGE => 'https://auth.stage.koalityengine.com',
+        self::ENVIRONMENT_PRODUCTION => 'https://auth.koalityengine.com'
     ];
 
     /**
