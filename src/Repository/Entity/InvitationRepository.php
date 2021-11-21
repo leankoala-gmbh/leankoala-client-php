9701f9be6229da99baccd4787ebf8ec7c047b746
Nils Langner <nils.langner@leankoala.com>
2021-11-21 10:55:56 +0100

    rm user_role as mandatory

--- src/Repository/Entity/InvitationRepository.php
+++ src/Repository/Entity/InvitationRepository.php
@@ -9,25 +9,24 @@ use Leankoala\ApiClient\Repository\Repository;
  *
  * All changes made in this file will be overwritten by the next create run.
  *
- * @created 2021-05-06
+ * @created 2021-11-21
  */
 class InvitationRepository extends Repository  {
 
   /**
    * This endpoint invites a new user to the project.
    *
-   * @param project
-   * @param {Object} args
+   * @param $project
+   * @param array $args
    * @param {String} args.email The invitations e-mail address
    * @param {String} args.user_name The users company name. (default: )
-   * @param {Number} args.user_role The projects role of the newly added user.
-   * @param {Number} args.inviter The inviters user id.
+   * @param {Integer} args.user_role The projects role of the newly added user. (default: 200)
    */
-  public function invite($project, $args)
+  public function invite($project, array $args)
   {
     $route = ['path' => 'user/invitation/invite/{project}', 'method' => 'POST', 'version' =>  1];
     $argList = array_merge(['project' => $project], $args);
-    $requiredArguments = ['email', 'user_role', 'inviter'];
+    $requiredArguments = ['email'];
     $this->assertValidArguments($requiredArguments, $argList);
 
     return $this->connection->send($route, $argList);
@@ -36,10 +35,10 @@ class InvitationRepository extends Repository  {
   /**
    * This endpoint aborts a given invitation.
    *
-   * @param invitation
-   * @param {Object} args
+   * @param $invitation
+   * @param array $args
    */
-  public function abort($invitation, $args)
+  public function abort($invitation, array $args)
   {
     $route = ['path' => 'user/invitation/abort/{invitation}', 'method' => 'DELETE', 'version' =>  1];
     $argList = array_merge(['invitation' => $invitation], $args);
@@ -50,10 +49,10 @@ class InvitationRepository extends Repository  {
   /**
    * This endpoint returns a list of all open invitations for the given project.
    *
-   * @param project
-   * @param {Object} args
+   * @param $project
+   * @param array $args
    */
-  public function getOpenInvitations($project, $args)
+  public function getOpenInvitations($project, array $args)
   {
     $route = ['path' => 'user/invitation/open/{project}', 'method' => 'GET', 'version' =>  1];
     $argList = array_merge(['project' => $project], $args);
