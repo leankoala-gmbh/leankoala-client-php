7c1aadf203f46cda27a547a4f8823e02505a72a8
Nils Langner <nils.langner@phmlabs.com>
2022-03-21 09:37:02 +0100

    getBlockedPatterns

--- src/Repository/Entity/CheckDeadLinksRepository.php
+++ src/Repository/Entity/CheckDeadLinksRepository.php
@@ -11,82 +11,95 @@ use Leankoala\ApiClient\Repository\Repository;
  *
  * @created 2021-05-06
  */
-class CheckDeadLinksRepository extends Repository  {
+class CheckDeadLinksRepository extends Repository
+{
 
-  /**
-   * Return a list of dead links for every component attached to the given system.
-   *
-   * @param system
-   * @param {Object} args
-   */
-  public function getResults($system, $args)
-  {
-    $route = ['path' => 'check/checks/{system}/deadlinks', 'method' => 'GET', 'version' =>  1];
-    $argList = array_merge(['system' => $system], $args);
+    /**
+     * Return a list of dead links for every component attached to the given system.
+     *
+     * @param system
+     * @param {Object} args
+     */
+    public function getResults($system, $args)
+    {
+        $route = ['path' => 'check/checks/{system}/deadlinks', 'method' => 'GET', 'version' => 1];
+        $argList = array_merge(['system' => $system], $args);
 
-    return $this->connection->send($route, $argList);
-  }
+        return $this->connection->send($route, $argList);
+    }
 
-  /**
-   * Return a list of dead links for every component in every project owned by the company.
-   *
-   * @param company
-   * @param {Object} args
-   */
-  public function getResultsByCompany($company, $args)
-  {
-    $route = ['path' => 'check/checks/company/{company}/deadlinks', 'method' => 'GET', 'version' =>  1];
-    $argList = array_merge(['company' => $company], $args);
+    /**
+     * Return a list patterns that should be blocked in the dead link checker.
+     * @param array $args
+     */
+    public function getBlockedPatterns(array $args = [])
+    {
+        $route = ['path' => 'check/checks/deadlinks/blocked', 'method' => 'GET', 'version' => 1];
+        $argList = array_merge([], $args);
 
-    return $this->connection->send($route, $argList);
-  }
+        return $this->connection->send($route, $argList);
+    }
 
-  /**
-   * Return the dead link check configuration for the given system.
-   *
-   * @param system
-   * @param {Object} args
-   */
-  public function getConfiguration($system, $args)
-  {
-    $route = ['path' => 'check/checks/{system}/deadlinks/config', 'method' => 'GET', 'version' =>  1];
-    $argList = array_merge(['system' => $system], $args);
+    /**
+     * Return a list of dead links for every component in every project owned by the company.
+     *
+     * @param company
+     * @param {Object} args
+     */
+    public function getResultsByCompany($company, $args)
+    {
+        $route = ['path' => 'check/checks/company/{company}/deadlinks', 'method' => 'GET', 'version' => 1];
+        $argList = array_merge(['company' => $company], $args);
 
-    return $this->connection->send($route, $argList);
-  }
+        return $this->connection->send($route, $argList);
+    }
 
-  /**
-   * Add a new ignore pattern to the configuration.
-   *
-   * @param system
-   * @param {Object} args
-   * @param {Array} args.patterns List of URLs (strings) that will be excluded from the dead link crawl
-   */
-  public function ignorePattern($system, $args)
-  {
-    $route = ['path' => 'check/checks/{system}/deadlinks/ignore', 'method' => 'POST', 'version' =>  1];
-    $argList = array_merge(['system' => $system], $args);
-    $requiredArguments = ['patterns'];
-    $this->assertValidArguments($requiredArguments, $argList);
+    /**
+     * Return the dead link check configuration for the given system.
+     *
+     * @param system
+     * @param {Object} args
+     */
+    public function getConfiguration($system, $args)
+    {
+        $route = ['path' => 'check/checks/{system}/deadlinks/config', 'method' => 'GET', 'version' => 1];
+        $argList = array_merge(['system' => $system], $args);
 
-    return $this->connection->send($route, $argList);
-  }
+        return $this->connection->send($route, $argList);
+    }
 
-  /**
-   * Remove an ignore pattern from the configuration.
-   *
-   * @param system
-   * @param {Object} args
-   * @param {Number} args.pattern_id Single URL that will not be excluded anymore in the dead link crawl
-   */
-  public function unignorePattern($system, $args)
-  {
-    $route = ['path' => 'check/checks/{system}/deadlinks/unignore', 'method' => 'POST', 'version' =>  1];
-    $argList = array_merge(['system' => $system], $args);
-    $requiredArguments = ['pattern_id'];
-    $this->assertValidArguments($requiredArguments, $argList);
+    /**
+     * Add a new ignore pattern to the configuration.
+     *
+     * @param system
+     * @param {Object} args
+     * @param {Array} args.patterns List of URLs (strings) that will be excluded from the dead link crawl
+     */
+    public function ignorePattern($system, $args)
+    {
+        $route = ['path' => 'check/checks/{system}/deadlinks/ignore', 'method' => 'POST', 'version' => 1];
+        $argList = array_merge(['system' => $system], $args);
+        $requiredArguments = ['patterns'];
+        $this->assertValidArguments($requiredArguments, $argList);
 
-    return $this->connection->send($route, $argList);
-  }
+        return $this->connection->send($route, $argList);
+    }
+
+    /**
+     * Remove an ignore pattern from the configuration.
+     *
+     * @param system
+     * @param {Object} args
+     * @param {Number} args.pattern_id Single URL that will not be excluded anymore in the dead link crawl
+     */
+    public function unignorePattern($system, $args)
+    {
+        $route = ['path' => 'check/checks/{system}/deadlinks/unignore', 'method' => 'POST', 'version' => 1];
+        $argList = array_merge(['system' => $system], $args);
+        $requiredArguments = ['pattern_id'];
+        $this->assertValidArguments($requiredArguments, $argList);
+
+        return $this->connection->send($route, $argList);
+    }
 
 }
